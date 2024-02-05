3a0fdbb862fda2350d0691f8cd4915641948f204
Domonkos Kertesz <d.kertesz16@gmail.com>
2024-02-05 15:46:22 +0100

    Changed writing to JSON to writing to HTML

--- src/WriteToFile.java
+++ src/WriteToFile.java
@@ -5,20 +5,20 @@ public class WriteToFile {
 	private WriteToFile(){}
 
 	public static void WriteSingleHtmlEntry(String entry){
-		try (FileWriter writer = new FileWriter("izzie.json")) {
+		try (FileWriter writer = new FileWriter("izzie.html")) {
 			writer.write(entry);
-			System.out.println("Entry written to izzy.json");
+			System.out.println("Entry written to izzy.html");
 		} catch (IOException e) {
 			System.out.println("An error occurred: " + e.getMessage());
 		}
 	}
 
 	public static void WriteAllHtmlEntries(String[] entries){
-		try (FileWriter writer = new FileWriter("jsondata.json")) {
+		try (FileWriter writer = new FileWriter("jsondata.html")) {
 			for (String entry : entries){
 				writer.write(entry);
 			}
-			System.out.println("Entries written to jsondata.json");
+			System.out.println("Entries written to jsondata.html");
 		} catch (IOException e) {
 			System.out.println("An error occurred: " + e.getMessage());
 		}
